bd57b80bb7873a1ba4141d3023bf16427b591b87
privalovsa <privalovsa@it.mos.ru>
2022-03-23 17:57:10 +0300

    debounce / throttling

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
     <h1>Tasks from interview</h1>
     <div className='content'>
       <ManageInputs />
-      <Refs />
+      {/* <Refs /> */}
     </div>
 
 
--- src/components/ManageInputs.tsx
+++ src/components/ManageInputs.tsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+import { debounce } from "ts-debounce";
 
 /**
  * TODO
@@ -12,21 +14,46 @@ import React, { useState } from 'react'
 const validate = (item :any) => item === 'react'
 
 const ManageInputs = () => {
+    const [values, setValues] = useState(['vue', 'angular']);
 
-    const [values, setValues] = useState([ 'vue', 'angular', 'react' ]);
+    function throttle(func: any, timeFrame: any) {
+        var lastTime = 0;
+        return function () {
+            var now = Date.now();
+            if (now - lastTime >= timeFrame) {
+                func();
+                lastTime = now;
+            }
+        };
+      }
 
-    const onChange = (e :any, index :any) => {
-        const newValue = [...values];
-        newValue[index] = e.currentTarget.value;
-        setValues(newValue)
-    }
+      //Пробный запрос
+    async function fetch(){
+        try{
+          const response: any = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10');
+          console.log('response :>> ', response);
+        } catch (e) {
+          alert(e)
+        }
+      }
+      //Срабатывает сразу, следующий вызов через указанное время
+      const f = throttle(fetch, 3000)
+
+      //Срабатывает после истечения времени и если прервалось ожидание, то не выполниться
+    //   const f = debounce(fetch, 3000)
 
     const onClick = () => {
-        const inputs = [...values]
-        inputs.push(' ')
-        setValues(inputs)
-        console.log('values :>> ', values);
-      }
+        const newValues = [...values]
+        newValues.push('');
+        setValues(newValues);
+    }
+
+    const onChange: any = (e: any, index: any) => {
+        const newValues = [...values]
+        newValues[index] = e.currentTarget.value;
+        setValues(newValues);
+    }
+      
     return (
         <form>
             <h3>Rambler. Input Managment</h3>
@@ -37,18 +64,20 @@ const ManageInputs = () => {
                 * Если input value везде "react", то кнопка "Сохранить"
                 * должна стать disabled='false'
                 * </h5>
-            {values.map((val, index) => <input
+           {values.map((val, index)=> <input
                 key={index}
-                onChange={(e)=> onChange(e, index)}
+                onChange={(e) => onChange(e, index)}
+                onMouseMove={(e)=> f()}//Изучаем debounce И throttling по движению мыши. отправка запроса
                 className='input'
                 type='text'
                 defaultValue={val}
+                placeholder='ВВЕДИ'
             />)}
             <div className='controls'>
                 <button
                     className='button'
-                    onClick={onClick}
                     type='button'
+                    onClick={onClick}
                 >
                     Добавить Input
                 </button>
@@ -56,7 +85,6 @@ const ManageInputs = () => {
                     className='button'
                     type='button'
                     disabled={!values.every(validate)}
-                    onClick={() => alert('You Win Samurai !!!')}
                     >
                     Создать
                 </button>
